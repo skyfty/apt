0fbf1852cbfdbec10ef824c1c68f92d4dc1f43cb
feitianyu <feitianyu@maxthon.net>
2021-08-02 21:40:57 +0800

    初始化网站结构

--- application/common/model/Miniaturemodel.php
+++ application/common/model/Miniaturemodel.php
@@ -9,18 +9,7 @@ class Miniaturemodel extends Cosmetic
     protected $name = 'miniaturemodel';
     public $keywordsFields = ["name", "idcode"];
 
-    protected static function init()
-    {
-        $beforeupdate = function($row){
-            if (isset($row['name'])) {
-                try {
-                    $row['slug'] = \fast\Pinyin::get($row['name']);
-                } catch(\Exception $e) {
-                }
-            }
-        };
-        self::beforeInsert($beforeupdate);self::beforeUpdate($beforeupdate);
-    }
+
     public function promotion() {
         return $this->hasOne('promotion','id','promotion_model_id')->joinType("LEFT")->setEagerlyType(0);
     }
