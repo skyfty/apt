8c1caa0cca6c57bc9ff3340e2c0f6b65a637ab69
feitianyu <feitianyu@maxthon.net>
2022-09-28 21:05:28 +0800

    初始化网站结构

---
+++ application/bi/controller/Verify.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace app\bi\controller;
+
+use app\common\controller\Api;
+use think\Request;
+
+/**
+ * 首页接口
+ */
+class Verify extends Api
+{
+    protected $noNeedLogin = ['*'];
+    protected $noNeedRight = ['*'];
+
+    public function index() {
+        $code = $this->request->param('code');
+        if ($code == null) {
+            $this->error("game id error");
+            return;
+        }
+        $verify = model("verify")->where("name", $code)->find();
+        if ($verify == null || $verify['status'] == "disallow") {
+            $this->error("disallow");
+        }
+        $this->success();
+    }
+}
