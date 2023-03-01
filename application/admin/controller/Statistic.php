912ea3a4031a7d1fee16c449a1c822a31bc4f78d
skyfty <373388688@qq.com>
2023-03-01 19:57:24 +0800

    dsfsdf

--- application/admin/controller/Statistic.php
+++ application/admin/controller/Statistic.php
@@ -2,7 +2,9 @@
 
 namespace app\admin\controller;
 
+use app\admin\model\Modelx;
 use app\common\controller\Backend;
+use think\App;
 
 /**
  * 图片资源
@@ -14,7 +16,7 @@ class Statistic extends Cosmetic
     public function _initialize()
     {
         parent::_initialize();
-        $this->model = new \app\admin\model\Picture;
+        $this->model = new \app\admin\model\Cause;
     }
 
 
@@ -23,4 +25,29 @@ class Statistic extends Cosmetic
     }
 
 
+    public function index() {
+        $this->request->filter(['strip_tags']);
+
+        $cosmeticModel = Modelx::get(['table' => "cause"],[],!App::$debug);
+
+        if ($this->request->isAjax()) {
+            $relationSearch = $this->getRelationSearch($cosmeticModel);;
+            list($where, $sort, $order, $offset, $limit) = $this->buildparams();
+            $this->model->alias("cause")->where($where)->with($relationSearch);
+            $total = $this->model->count();
+
+            $this->model->alias("cause")->where($where)->with($relationSearch)->order($sort, $order)->limit($offset, $limit);
+            $list = $this->model->select();
+
+            $relationModel = $this->getRelationModel($cosmeticModel);;
+            foreach($list as $row) {
+                foreach($relationModel as $rmk=>$rm) {
+                    $row->appendRelationAttr($rmk, $rm);
+                }
+            }
+            return json(array("total" => $total, "rows" => collection($list)->toArray()));
+        }
+        $this->assignScenery($cosmeticModel->id, ['index']);
+        return $this->view->fetch("index");
+    }
 }
--- public/assets/js/backend/statistic.js
+++ public/assets/js/backend/statistic.js
@@ -4,6 +4,34 @@ define(['jquery', 'backend', 'table', 'form','template','angular','cosmetic'], f
         lands:{
             index:function($scope, $compile,$timeout, data) {
 
+                $scope.filter = function() {
+                    console.log("lskdjf");
+                    Controller.api.open
+                };
+
+                $scope.searchFieldsParams = function(param) {
+                    param.custom = {};
+                    return param;
+                };
+                var options = {
+                    search: false, //是否启用快速搜索
+                    cache: false,
+                    commonSearch: false, //是否启用通用搜索
+                    searchFormVisible: false, //是否始终显示搜索表单
+                    extend: {
+                        index_url: 'cause/index',
+                        add_url: '',
+                        del_url: '',
+                        multi_url: '',
+                        summation_url: '',
+                        table: 'cause',
+                    },
+                    buttons : []
+                };
+                Table.api.init(options);
+                Form.api.bindevent($("div[ng-controller='index']"));
+
+
             }
         },
         viewscape:function($scope, $compile,$parse, $timeout){
